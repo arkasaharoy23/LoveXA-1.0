016c79c0a8f70e451a587a4cf374c0925e9fb53e
arkasaharoy23 <149593413+arkasaharoy23@users.noreply.github.com>
2026-05-30 21:48:48 +0530

    .

--- backend/routes/proposal-routes.js
+++ backend/routes/proposal-routes.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const uploadMiddleware = require('../middleware/uploadMiddleware');
+const uploadMiddleware = require('../middleware/error-middleware');
 
 const {
   createProposal,
